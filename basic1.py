23f0bccf2addc5d0cc9f1d99fabd56868e6c96ff
adii238 <adityab2381@gmail.com>
2021-10-28 14:16:32 -0400

    basic1.py

---
+++ basic1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+student_grades = [9.1, 8.8, 7.5]   #variable proviiding 3 students grades.
+max_value = max(student_grades)    # the "max" function calclutes the maximum of the given list and returns it.
+print(max_value)
